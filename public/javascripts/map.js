b82cdac7d1dd4884909ee96702b69e09967bdb78
Hunter Thomas <archangelq@gmail.com>
2011-05-18 22:35:02 -0700

    And removing the map.js from git so it gets regenerated

--- public/javascripts/map.js
+++
@@ -1,9 +0,0 @@
-/* DO NOT MODIFY. This file was compiled Thu, 19 May 2011 04:51:13 GMT from
- * /home/archangelq/Projects/HandCannonCommunity/app/coffeescripts/map.coffee
- */
-
-(function() {
-  $(document).ready(function() {
-    return alert("Coffeescript enabled!");
-  });
-}).call(this);
