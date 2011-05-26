9bbe5df80b28712e2df50c5af5c8ae278d85cb57
Hunter Thomas <archangelq@gmail.com>
2011-05-25 21:14:54 -0700

    Added map clustering support, yay

--- app/coffeescripts/map.coffee
+++ app/coffeescripts/map.coffee
@@ -1,5 +1,13 @@
 $(document).ready ->
-  $('#map').jMapping()
+  $('#map').jMapping
+      map_config:
+        navigationControlOptions:
+          style: google.maps.NavigationControlStyle.DEFAULT
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 7
+  mapdata = $('#map').data('jMapping')
+  window.mapdata = mapdata
+  mc = new MarkerClusterer(mapdata.map, mapdata.gmarkersArray())
   $("#search").bind "ajax:success", (event, data, status, xhr) -> 
       d = $.parseJSON data
       $("#flash").html d.errors
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,10 +1,22 @@
-/* DO NOT MODIFY. This file was compiled Thu, 26 May 2011 01:56:33 GMT from
+/* DO NOT MODIFY. This file was compiled Thu, 26 May 2011 04:11:46 GMT from
  * /home/archangelq/Projects/HandCannonCommunity/app/coffeescripts/map.coffee
  */
 
 (function() {
   $(document).ready(function() {
-    $('#map').jMapping();
+    var mapdata, mc;
+    $('#map').jMapping({
+      map_config: {
+        navigationControlOptions: {
+          style: google.maps.NavigationControlStyle.DEFAULT
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoom: 7
+      }
+    });
+    mapdata = $('#map').data('jMapping');
+    window.mapdata = mapdata;
+    mc = new MarkerClusterer(mapdata.map, mapdata.gmarkersArray());
     return $("#search").bind("ajax:success", function(event, data, status, xhr) {
       var d;
       d = $.parseJSON(data);
